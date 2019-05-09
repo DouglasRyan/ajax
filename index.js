e92708bc19801d625db49e933802d3c0557953d6
ryan <2869888186@qq.com>
2019-05-09 15:10:04 +0800

    jQuery  jsonp

--- index.js
+++ index.js
@@ -36,9 +36,8 @@ var server = http.createServer(function(request, response) {
         response.setHeader('Content-Type', 'application/javascript')
         response.statusCode = 200
         response.write(`
-          amount.innerText = amount.innerText - 1
-          console.log('1')
-          `)
+          ${query.callback}.call(undefined,'success')
+        `)
         response.end()
     } else {
         response.statusCode = 404
